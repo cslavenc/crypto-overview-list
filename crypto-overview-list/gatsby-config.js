2cc2ab80e6f7a653a16f85814047ab0ab36db803
cslavenc <slavenc@hotmail.ch>
2022-01-30 20:25:07 +0100

    Add gatsby starter

---
+++ crypto-overview-list/gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pathPrefix: "/crypto-overview-list",
+  siteMetadata: {
+    title: `Overview of Cryptocurrencies`,
+    description: `Overview of crypto currencies and their sectors`,
+    author: `@cslavenc`,
+    siteUrl: `https://cslavenc.github.io/`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+  ],
+}
